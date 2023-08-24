765f8e511ed4b68a2bd1a96f9d0918ca94eb1005
Sumeet Jain <59311803+Sumjains@users.noreply.github.com>
2023-08-24 14:02:51 +0530

    Update oktaConfig.ts

--- Book Library Project/03-frontend/react-library/src/lib/oktaConfig.ts
+++ Book Library Project/03-frontend/react-library/src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
     clientId: '0oaawdofq55RPajTy5d7',
-    issuer: 'https://dev-06606713.okta.com/oauth2/default',
+    issuer: 'https://{issuer ID}/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
